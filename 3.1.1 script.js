2d232bd919a7f76f4f34ddbcbc5b37d6ee0868fc
ValeriaIbragimova <valeria92-92+1@mail.ru>
2024-03-13 22:15:05 +0300

    3.1.1 added script.js

---
+++ 3.1.1 script.js
@@ -0,0 +1,13 @@
+// Код из предыдущего д.з. Расчет бонусов. Условия - если sum > 50? То бонус = 50, в противном случае, бонус = sum
+
+const calculateBonus = (a, b) => {
+  let bonus;
+  const sum = a + b;
+  debugger;
+  sum > 50 ? (bonus = 50) : (bonus = sum);
+  debugger;
+  return bonus;
+};
+console.log(calculateBonus(20, 10));
+
+module.exports = calculateBonus; // функция будет доступна вне этого файла.
